f94529789f5067fc13aa3ac2faa2b3363b62c9c0
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-08-06 15:43:28 +0530

    Time: 22 ms (28.65%) | Memory: 50.7 MB (77.20%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,8 +1,5 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        // int m = ;
-        // int n = ;
-        // if(m * n < k) return 1;
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a - b);
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
@@ -13,14 +10,5 @@ class Solution {
             pq.poll();
         }return pq.poll();
     }
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter f = new FileWriter("display_runtime.txt")) {
-                f.write("0");
-            } catch (Exception e) {
-
-            }
-        }));
-    }
+    
 }
