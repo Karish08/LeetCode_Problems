db21e85f625839ccdb6d7f3134e5f510a6cf6f98
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-08-07 16:01:44 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (95.95%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -1,10 +1,15 @@
-import java.math.BigInteger;
+//  import java.math.BigInteger;
 class Solution {
     public String removeTrailingZeros(String num) {
-        StringBuilder sb = new StringBuilder(num);
-        sb.reverse();
-        BigInteger n = new BigInteger(sb.toString());
-        sb = new StringBuilder(String.valueOf(n));
-        return sb.reverse().toString();
+        int i = num.length()- 1;
+        while(i >= 0 && num.charAt(i) == '0'){
+            i--;
+        }
+        return num.substring(0, i+1);
+        // StringBuilder sb = new StringBuilder(num);
+        // sb.reverse();
+        // BigInteger n = new BigInteger(sb.toString());
+        // sb = new StringBuilder(String.valueOf(n));
+        // return sb.reverse().toString();
     }
 }
