4932759e4f6a123e7de06d10e0dcd3e019bc6b59
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-08-07 14:50:31 +0530

    Time: 12 ms (5.44%) | Memory: 48 MB (8.90%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -1,12 +1,10 @@
 class Solution {
     public int mostWordsFound(String[] sentences) { 
-        int kb[] = new int [sentences.length];
-        int i = 0;       
+        int max = 0;       
         for(String m : sentences){
             String ak[] = m.split("\\s+");
-            kb[i++] = ak.length;
-        }  
-        Arrays.sort(kb);
-        return kb[kb.length - 1];      
+            max = Math.max(max, ak.length);
+        } 
+        return max;      
     }
 }
