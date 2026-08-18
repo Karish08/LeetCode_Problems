c2bc56c22e0ca324ee28d0d9e980de3f3079d728
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-08-18 16:30:54 +0530

    Time: 33 ms (3.38%) | Memory: 270.3 MB (8.00%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
        Queue<Integer> ak = new LinkedList<>();
-       for(int i : nums){
-        ak.offer(i);
-       } 
        int n = nums.length;
        k = k % n;
-       k = (n - k);
-       for(int i = 0; i<k; i++){
-          int m = ak.poll();  
-          ak.offer(m);
+       for(int i = n - k; i < n; i++){ 
+          ak.offer(nums[i]);
+       }
+       for(int i = 0; i < n - k; i++){ 
+          ak.offer(nums[i]);
        }
        for(int i = 0; i<n; i++){
           nums[i] = ak.poll();
