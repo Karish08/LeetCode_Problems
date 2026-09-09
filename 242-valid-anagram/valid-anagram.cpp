e3ebca2e30b0b59e5f083d79c0cdfc9bfa0687dc
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-09-09 15:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (25.71%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq1(26);
+        vector<int> freq2(26);
+        for(int i = 0;i < s.size(); i++){
+            freq1[s[i] - 'a']++;
+        }
+        for(int i = 0;i < t.size(); i++){
+            freq2[t[i] - 'a']++;
+        }
+        for(int i = 0; i<26; i++){
+            if(freq1[i] < freq2[i] || freq1[i] > freq2[i]) return false;
+        }
+        return true;
+    }
+};
