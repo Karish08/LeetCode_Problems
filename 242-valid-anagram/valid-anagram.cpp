65384f20e38b4171faa240b773af325c3a08e5d0
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-09-12 13:57:39 +0530

    Time: 4 ms (33.85%) | Memory: 9.7 MB (75.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> ak;
-        map<char, int> kb;
-        for(char c : s){
-            ak[c]++;
-        }
-        for(char c : t){
-            kb[c]++;
-        }
-        for(char c : s){
-            if(ak[c] != kb[c]) return false;
-        }
-        for(char c : t){
-            if(ak[c] != kb[c]) return false;
+        int freq[26] = {0};
+        for(char c : s) freq[c - 'a']++;
+        for(char c : t) freq[c - 'a']--;
+        for(int i = 0; i<26; i++){
+            if(freq[i] != 0) return false;
         }
         return true;
     }
