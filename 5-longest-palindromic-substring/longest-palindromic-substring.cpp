47c90039613e0a5a97af9d946773e954689d96a9
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-09-20 10:18:55 +0530

    Time: 19 ms (41.52%) | Memory: 22 MB (31.69%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ak = "";
-        for(int i = 0; i<s.size(); i++){
-            int left = i; int right = i;
-            while(left >= 0 && right <s.size() && s[left] == s[right]){
-                if(ak.size() < right - left + 1){
-                    ak = s.substr(left, right - left + 1);
-                }
-                left--;
-                right++;
+       string ak = "";
+       for(int i = 0; i<s.size(); i++){
+         int left = i;
+         int right =  i;
+         while(left >= 0 && right < s.size() && s[left] == s[right] ){
+            if(right - left +1 > ak.size()){
+                ak = s.substr(left, right - left + 1);
             }
-            left = i; right = i + 1;
-            while(left >= 0 && right <s.size() && s[left] == s[right]){
-                if(ak.size() < right - left + 1){
-                    ak = s.substr(left, right - left + 1);
-                }
-                left--;
-                right++;
+            left --;
+            right++;
+         }
+         left = i;
+        right =  i + 1;
+         while(left >= 0 && right < s.size() && s[left] == s[right] ){
+            if(right - left +1 > ak.size()){
+                ak = s.substr(left, right - left + 1);
             }
-        }return ak;
+            left --;
+            right++;
+         }
+       }return ak;
     }
 };
