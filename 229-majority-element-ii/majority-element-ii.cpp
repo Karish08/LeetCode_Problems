975cf9c126be33278613909bb3df32566ca3bac7
Karish Bhagavath <133204366+Karish08@users.noreply.github.com>
2026-09-24 15:46:50 +0530

    Time: 11 ms (19.26%) | Memory: 27.2 MB (6.13%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> mj;
+        int check = nums.size() / 3;
+        map<int, int> ak;
+        for(int i : nums){
+            ak[i]++;
+        }
+        for(auto var : ak){
+            if(var.second > check) mj.push_back(var.first);
+        }
+        return mj;
+    }
+};
